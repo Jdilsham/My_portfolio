11df793707e7e5f54113ed207dba60e62d20e02d
Jdilsham <janithadilsham@gmail.com>
2025-11-11 00:04:15 +0530

    add

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+// Force HTTPS on Heroku
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] !== 'https') {
+    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+}
+
+
 include_once("index.html");
 ?>
